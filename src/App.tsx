a985c6d30400cbe75adcae6b3bf9ffba2b411e58
Manuel <carlosd99m@gmail.com>
2021-04-23 17:31:24 -0600

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import * as React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
-  );
+export class App extends React.Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            tasks: []
+        };
+    }
+
+    render () {
+        return (
+            <h1>{this.props.title}</h1>
+        )
+    }
 }
 
-export default App;
+interface IProps {
+    title: string;
+}
+
+interface IState {
+    tasks: [];
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {App} from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <App  title={'Hola desde react-ts'}/>
     </React.StrictMode>,
     document.getElementById('root')
 );
